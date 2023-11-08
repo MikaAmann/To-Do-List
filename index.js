8cc5bd3637dc73c4f3e9826c5eae31fd3c293d3e
Mika Amann <mika.amann01@gmail.com>
2023-11-08 16:47:19 +0100

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove, set } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://to-do-list-d62af-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -10,28 +10,86 @@ const database = getDatabase(app)
 const toDoListInDB = ref(database, "toDoList")
 
 
-
-let taskNameEl = document.getElementById("add-name");
-let taskDateEl = document.getElementById("add-date");
-let addItemBtn = document.getElementById("add-btn");
-let taskList = document.getElementById("task-list");
-
-let listItems = []; 
+const taskNameEl = document.getElementById("add-name");
+const taskDateEl = document.getElementById("add-date");
+const addItemBtn = document.getElementById("add-btn");
+const taskList = document.getElementById("task-list");
 
 addItemBtn.addEventListener("click", function() {
+
+    if(taskNameEl.value === "" || taskDateEl.value === ""){
+        alert("Both fields must contain values!")
+        return
+    }
+
     let inputNameValue = taskNameEl.value
     let inputDateValue = taskDateEl.value
 
-    console.log("button pressed")
+    let newTask = taskToObject(inputNameValue, inputDateValue)
 
-    let newEl = document.createElement("li")
-    
-    newEl.textContent = `Name: ${inputNameValue}, Date: ${inputDateValue}`
-    
-    taskList.append(newEl)
+    let taskIDinDB = push(toDoListInDB, newTask).key
+
+    newTask.id = taskIDinDB
+
+    set(ref(database, 'toDoList/' +taskIDinDB), {
+        id: newTask.id,
+        name: newTask.name,
+        date: newTask.date,
+        state: newTask.state
+    });
+
+    clearInputFields();
 })
 
 
-//make input into object
 
-//function to render new object when added (also used to render all items from database)
+onValue(toDoListInDB, function(snapshot) {
+    
+    if(snapshot.exists()){
+
+        let itemsArray = Object.entries(snapshot.val())
+        
+        clearTaskListEl()
+    
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+    
+            appendItemToTaskListEl(currentItem)
+        }
+    }
+    else{
+        taskList.innerHTML = "No tasks here... yet"
+    }
+})
+
+
+function taskToObject(inputNameValue, inputDateValue){
+
+    let taskObj = {
+        id: "", //id set in firebase
+        name: inputNameValue,
+        date: inputDateValue,
+        state: "New"
+    }
+    return taskObj
+}
+
+function clearTaskListEl(){
+    taskList.innerHTML = ""
+}
+
+function clearInputFields(){
+    taskNameEl.value = ""
+    taskDateEl.value = ""
+}
+
+function appendItemToTaskListEl(item){
+    let itemObject = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = `Name: ${itemObject.name}, Date: ${itemObject.date}, Status: ${itemObject.state}`
+    
+    taskList.append(newEl)
+ 
+}
